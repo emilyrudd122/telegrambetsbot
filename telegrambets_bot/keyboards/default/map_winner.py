e52b53d896b2f1f541c4c9236167ac697205ee9d
Dan Smirnov <danuas@yandex.ru>
2020-12-17 17:52:06 +0300

    added option for bet on full game

--- telegrambets_bot/keyboards/default/map_winner.py
+++ telegrambets_bot/keyboards/default/map_winner.py
@@ -7,7 +7,10 @@ map_winner = ReplyKeyboardMarkup(
             KeyboardButton(text='map1'),
             KeyboardButton(text='map2'),
             KeyboardButton(text='map3'),
-        ]
+        ],
+        [
+            KeyboardButton(text='game'),
+        ],
     ],
     resize_keyboard=True
 )
